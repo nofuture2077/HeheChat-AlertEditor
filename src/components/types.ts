336c4b38c0ddd77556716977b68bb8d0f0224017
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2024-09-22 19:42:05 +0200

    new optional fields

--- src/components/types.ts
+++ src/components/types.ts
@@ -73,6 +73,9 @@ export type EventAlertTTS = {
 export type EventAlertVisualData = {
     element: Base64FileReference;
     text?: string;
+    position?: string;
+    layout?: string;
+    color?: string;
 }
 
 export type Base64FileReference = string;
