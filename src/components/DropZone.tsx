c60459ff256b1ca55dc58145f2a62899910ec422
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2025-08-06 11:50:50 +0200

    feat: webm aupport

--- src/components/DropZone.tsx
+++ src/components/DropZone.tsx
@@ -15,7 +15,7 @@ export function DropZone(props: {onSelect: (file: File) => void}) {
       }}
       onReject={(files) => console.log('rejected files', files)}
       maxSize={4 * 1024 ** 2}
-      accept={["audio/mpeg", "image/webp"]}
+      accept={["audio/mpeg", "image/webp", "video/webm"]}
       multiple={false}
     >
       <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: 'none' }}>
